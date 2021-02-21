d0d59882c89b3a5ede70245edd209d4713ce1ee6
Neelesh Kumar <kneelesh48@gmail.com>
2021-02-21 09:40:17 +0530

    doesn't download already downloaded files

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import ctypes
 import logging
 import platform
 import winsound
-import json
 from config import wallpaper_subs
 
-cd=os.path.dirname(__file__)
+cd=os.path.dirname(os.path.abspath(__file__))
 
 def logger_config():
     logger = logging.getLogger(__name__)
@@ -30,7 +29,7 @@ def wallpaper_changer():
     for _ in range(5): #if no image found, try another subreddit
         if break_loop: break
         sub=random.choice(wallpaper_subs)[0]
-        logger.info(sub)
+        logger.info(f"Subreddit: {sub}")
         data=requests.get(f"https://www.reddit.com/r/{sub}/top.json?t=day&limit=5",headers=headers).json()
         returned_len=len(data['data']['children'])
         n=random.randint(0,returned_len-1)
@@ -38,30 +37,40 @@ def wallpaper_changer():
             submission=data['data']['children'][(n+i)%returned_len]
             img_url=submission['data']['url']
             logger.info(img_url)
-            # if img_url[-1]!='/': #url links to image
-            if submission['data']['post_hint']=='image':
-                width=submission['data']['preview']['images'][0]['source']['width']
-                height=submission['data']['preview']['images'][0]['source']['height']
-                if width/height>=1 and (width>=1920 and height>=1080): #horizontal image
-                    logger.info('Downloading file')
-                    filepath=f"Images/{img_url.split('/')[-1]}"
-                    with open(filepath,'wb') as f:
-                        f.write(requests.get(img_url).content)
-                    logger.info('File downloaded!')
+            if img_url in open(f'{cd}/downloaded_links.txt').read().split('\n'): #File already downloaded
+                logger.info('File already downloaded')
+                continue
+            if submission['data']['post_hint']!='image': #url not image
+                logger.info('File not image')
+                continue
+            if img_url[-3:]=='gif': #Image is gif
+                logger.info('File is a gif')
+                continue
+            width=submission['data']['preview']['images'][0]['source']['width']
+            height=submission['data']['preview']['images'][0]['source']['height']
+            if width<=height: #vertical image
+                logger.info('Vertical')
+                continue
+            if not (width>=1280 and height>=720): #Low resolution
+                logger.info('Low resolution image')
+                continue
+            logger.info('Downloading file...')
+            filepath=f"Images/{img_url.split('/')[-1]}"
+            with open(filepath,'wb') as f:
+                f.write(requests.get(img_url).content)
+            with open(f'{cd}/downloaded_links.txt','a') as f:
+                print(img_url,file=f)
+            logger.info('File downloaded!')
 
-                    abs_img_path=f"{os.getcwd()}/{filepath}"
-                    if platform.system()=='Windows':
-                        ctypes.windll.user32.SystemParametersInfoW(20, 0, abs_img_path, 0)
-                        winsound.MessageBeep()
-                    else:
-                        logger.info('Not implemented')
-                    logger.info('Wallpaper Changed!\n')
-                    break_loop=True
-                    break
-                else:
-                    logger.info('Vertical')
+            abs_img_path=f"{os.getcwd()}/{filepath}"
+            if platform.system()=='Windows':
+                ctypes.windll.user32.SystemParametersInfoW(20, 0, abs_img_path, 0)
+                winsound.MessageBeep()
             else:
-                logger.info('Submission not image')
+                logger.info('Not implemented')
+            logger.info('Wallpaper Changed!\n')
+            break_loop=True
+            break
 
 if __name__ == '__main__':
     try:

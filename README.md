4ede8e5c70b112a4c4dadd4a40b87f12dc5cb901
Neelesh Kumar <kneelesh48@gmail.com>
2021-06-08 21:37:33 +0530

    Updated README

--- README.md
+++ README.md
@@ -4,4 +4,12 @@
 
 Downloads wallpapers from top wallpaper subreddits, chosen randomly and sets it as desktop wallpaper.
 
-Setup with Task Scheduler to change wallpaper at scheduled intervals automatically
+&#x200B;
+
+* Includes powershell script to add program to Task scheduler
+* Includes powershell script to add a desktop shortcut
+
+## Installation
+* Ensure that you have python3 installed and present in $PATH. Run `python --version` in powershell to check
+* Download the code and place the folder is a suitable place
+* Run `setup.ps1` in powershell. This will install required python packages, add a desktop shortcut and add the program to Task Scheduler to run everyday at 9 am.
--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,21 @@
+# Install required packages
+pip install -r requirements.txt
+
+# Create desktop shortcut
+$ShortcutFile="$($home)\Desktop\wallpaper changer.lnk"
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $(get-command pythonw).Source
+$Shortcut.Arguments = ".\main.py"
+$Shortcut.WorkingDirectory = $(Get-Location).Path
+$Shortcut.IconLocation = "$(Get-Location)\icon.ico"
+$Shortcut.Description = "Wallpaper Changer"
+$Shortcut.Save()
+
+Write-Output "Finished creating desktop shortcut"
+
+# Add program to Task Scheduler
 $taskName = "Wallpaper changer"
 $trigger =  New-ScheduledTaskTrigger -Daily -At 9am
 $action = New-ScheduledTaskAction -Execute 'C:\ProgramData\Anaconda3\pythonw.exe' -Argument '.\main.py' -WorkingDirectory $(Get-Location)

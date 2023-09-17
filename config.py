4f76fbe6d1cd856b9a27a5ee1d17788225bed3a1
Neelesh Kumar <kneelesh48@gmail.com>
2023-09-17 20:52:25 +0530

    Reformat config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-wallpaper_subs=[
-('EarthPorn', 20820398),
- ('NatureIsFuckingLit',4541833),
- ('wallpaper', 1617000),
-#  ('SpacePorn', 1391019),
- ('CityPorn', 658981),
- ('wallpapers', 605178),
-#  ('SkyPorn', 179753),
-#  ('BotanicalPorn', 141254),
-#  ('WaterPorn', 96425),
-#  ('VillagePorn', 73553),
- ('BeachPorn', 56079),
- ('WeatherPorn', 50865),
-#  ('multiwall', 27388),
-#  ('LakePorn', 7427)
+wallpaper_subs = [
+    ("EarthPorn", 20820398),
+    ("NatureIsFuckingLit", 4541833),
+    ("wallpaper", 1617000),
+    #  ('SpacePorn', 1391019),
+    ("CityPorn", 658981),
+    ("wallpapers", 605178),
+    #  ('SkyPorn', 179753),
+    #  ('BotanicalPorn', 141254),
+    #  ('WaterPorn', 96425),
+    #  ('VillagePorn', 73553),
+    ("BeachPorn", 56079),
+    ("WeatherPorn", 50865),
+    #  ('multiwall', 27388),
+    #  ('LakePorn', 7427)
 ]
